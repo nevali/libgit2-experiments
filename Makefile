9f3223c53c55e367554253de25f3dd8778af6334
Mo McRoberts <mo@nevali.net>
2013-04-14 23:20:42 +0100

    Add getall

--- Makefile
+++ Makefile
@@ -6,18 +6,24 @@ LISTBRANCH_OBJ = list-branches.o
 LISTTAG_OUT = listtag
 LISTTAG_OBJ = list-tags.o
 
+GETALL_OUT = getall
+GETALL_OBJ = config-getall.o
+
 CFLAGS = -I$(LIBGIT2_PREFIX)/include -W -Wall -O0 -ggdb
 LDFLAGS = -L$(LIBGIT2_PREFIX)/lib
 LIBS = -lgit2
 
-all: $(LISTBRANCH_OUT) $(LISTTAG_OUT)
+all: $(LISTBRANCH_OUT) $(LISTTAG_OUT) $(GETALL_OUT)
 	
 clean:
-	rm -f $(LISTBRANCH_OUT) $(LISTTAG_OUT)
-	rm -f $(LISTBRANCH_OBJ) $(LISTTAG_OBJ)
+	rm -f $(LISTBRANCH_OUT) $(LISTTAG_OUT) $(GETALL_OUT)
+	rm -f $(LISTBRANCH_OBJ) $(LISTTAG_OBJ) $(GETALL_OBJ)
 
 $(LISTBRANCH_OUT): $(LISTBRANCH_OBJ)
 	$(CC) $(LDFLAGS) -o $@ $+ $(LIBS)
 
 $(LISTTAG_OUT): $(LISTTAG_OBJ)
+	$(CC) $(LDFLAGS) -o $@ $+ $(LIBS)
+
+$(GETALL_OUT): $(GETALL_OBJ)
 	$(CC) $(LDFLAGS) -o $@ $+ $(LIBS)
---
+++ config-getall.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <git2.h>
+
+static int
+config_callback(const char *value, void *data)
+{
+	(void) data;
+	
+	puts(value);
+	return 0;
+}
+
+static void
+usage(const char *progname)
+{
+	fprintf(stderr, "Usage: %s VAR [PATH-TO-REPO]\nHonours GIT_DIR if set.\n", progname);
+}
+
+int
+main(int argc, char **argv)
+{
+	char *pathbuf;
+	size_t size;
+	const char *path;
+	const git_error *err;
+	git_repository *repo;
+	git_config *cfg;
+	
+	if(argc == 3)
+	{
+		path = argv[2];
+	}
+	else if(argc != 2)
+	{
+		usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	if(!path)
+	{
+		path = getenv("GIT_DIR");
+	}
+	pathbuf = NULL;
+	if(!path)
+	{
+		size = (size_t) pathconf(".", _PC_PATH_MAX);
+		pathbuf = (char *) malloc(size + 1);
+		if(!pathbuf)
+		{
+			perror(argv[0]);
+			exit(EXIT_FAILURE);
+		}
+		if(git_repository_discover(pathbuf, size + 1, ".", 0, "/"))
+		{
+			err = giterr_last();
+			free(pathbuf);
+			fprintf(stderr, "%s: %s\n", path, err->message);
+			exit(EXIT_FAILURE);
+		}
+		path = pathbuf;
+	}
+	if(git_repository_open(&repo, path))
+	{
+		err = giterr_last();
+		fprintf(stderr, "%s: %s\n", path, err->message);
+		exit(EXIT_FAILURE);
+	}
+	if(git_repository_config(&cfg, repo))
+	{
+		err = giterr_last();
+		fprintf(stderr, "%s: %s\n", path, err->message);
+		exit(EXIT_FAILURE);		
+	}
+	git_config_get_multivar(cfg, argv[1], NULL, config_callback, NULL);
+	git_config_free(cfg);
+	git_repository_free(repo);
+	free(pathbuf);
+	return 0;
+}
